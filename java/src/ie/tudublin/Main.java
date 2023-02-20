0e9d9cea8ff4c1654b471b268b9028a6c801af80
Kevin Ye <C21421254@mytudublin.ie>
2023-02-20 15:45:17 +0000

    changing cat file

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -29,7 +29,7 @@ public class Main
 	
 	public static void main(String[] args)
 	{
-		System.out.println("Hello world");
+		System.out.println("Hello");
 
 		Cat ginger = new Cat();
 		ginger.setName("Ginger");
