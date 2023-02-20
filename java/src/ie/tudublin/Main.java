45c7f36af7124bf52805cffa6691cb281fce1ad3
kevinye5 <c21421254@mytudublin.ie>
2023-02-20 13:31:28 +0000

    killing the cat

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -3,33 +3,36 @@ package ie.tudublin;
 public class Main
 {
 
-	public static void helloProcessing()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new HelloProcessing());
-    }
+	// public static void HelloProcessing()
+	// {
+	// 	String[] a = {"MAIN"};
+    //     processing.core.PApplet.runSketch( a, new HelloProcessing());
+    // }
 
-	public static void loops()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Loops());
-    }
+	// public static void Loops()
+	// {
+	// 	String[] a = {"MAIN"};
+    //     processing.core.PApplet.runSketch( a, new Loops());
+    // }
 
-	public static void bugZap()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new BugZap());
-    }
+	// public static void BugZap()
+	// {
+	// 	String[] a = {"MAIN"};
+    //     processing.core.PApplet.runSketch( a, new BugZap());
+    // }
 	
 	public static void main(String[] args)
 	{
 		System.out.println("Hello world");
-		
-		Dog penny = new Dog();
-		penny.setName("Penny");
-		penny.speak();
+		Cat ginger = new Cat();
+		ginger.setName("ginger");
 
-		loops();
+		for(int i = 0; i <= 9; i++)
+		{
+			ginger.lives();
+		}
+
+		//loops();
 	}
 	
 }
